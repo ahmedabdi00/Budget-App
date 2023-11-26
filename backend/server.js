45e5ede03c210f08b9dbbf62a594acb4d3e4533f
Ahmed A <a.abdi11965@gmail.com>
2023-11-25 20:09:03 -0500

    Re-org Front/Back(end)

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// server/server.js
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
